a4c2814e24ba4b84b871c96efeea2067af0cb47e
Nachiket Keripale <keripalenachiket@gmail.com>
2020-07-24 13:42:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Nachiket", "John","Pamya", "Jason", "Ajay", "Akshay","Jay", "Aditya",  "Chinmay", "Johnny "];
+    var names = ["Yaakov","John","Jen","Jason","Paul", "Frank","Larry","Paula","Laura","Jim"];
     for (var i = 0; i < names.length; i++) {
        var first = names[i].charAt(0);
         if (first === 'j' || first === 'J') {
